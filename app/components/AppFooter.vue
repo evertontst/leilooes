e758e18f9da95fef5ef916343c032279e20929b8
Everton Luis <131709674+gsm-everton@users.noreply.github.com>
2024-09-04 08:47:52 -0300

    Fix broken deploy

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-const title = useRuntimeConfig().app.name
+// const title = useRuntimeConfig().app.name
 
-const appConfig = useAppConfig()
-const notice = appConfig.footer.notice
-const smallLinks = appConfig.footer.smallLinks
-const socials = appConfig.socials
+// const appConfig = useAppConfig()
+// const notice = appConfig.footer.notice
+// const smallLinks = appConfig.footer.smallLinks
+// const socials = appConfig.socials
 </script>
 
 <template>
--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,35 +1,35 @@
 <script lang="ts" setup>
-const { $csrfFetch } = useNuxtApp()
+// const { $csrfFetch } = useNuxtApp()
 
-const { loggedIn, session, user } = useUserSession()
+// const { loggedIn, session, user } = useUserSession()
 
-const items = [
-  [
-    {
-      label: 'Profile',
-      to: '/profile',
-      icon: 'i-ph-user-duotone',
-    },
-  ],
-  [
-    {
-      label: 'Logout',
-      icon: 'i-ph-sign-out-duotone',
-      click: async () => {
-        await $csrfFetch('/api/_auth/session', {
-          method: 'DELETE',
-        })
+// const items = [
+//   [
+//     {
+//       label: 'Profile',
+//       to: '/profile',
+//       icon: 'i-ph-user-duotone',
+//     },
+//   ],
+//   [
+//     {
+//       label: 'Logout',
+//       icon: 'i-ph-sign-out-duotone',
+//       click: async () => {
+//         await $csrfFetch('/api/_auth/session', {
+//           method: 'DELETE',
+//         })
 
-        session.value = {}
+//         session.value = {}
 
-        await navigateTo('/')
-      },
-    },
-  ],
-]
+//         await navigateTo('/')
+//       },
+//     },
+//   ],
+// ]
 
-const title = useRuntimeConfig().app.name
-const icon = useAppConfig().app.logo
+// const title = useRuntimeConfig().app.name
+// const icon = useAppConfig().app.logo
 </script>
 
 <template>
